8b6547f6984ca159c03d55da6e6cfae7bea2a896
lekoeme <alexxxmuniz@gmail.com>
2024-03-28 23:15:14 -0300

    atualizando extensão index.php

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>SectorSolid3D</title>
+	 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container theme-showcase" role="main">
+		      <div class="page-header">  
+				<h1>SectorSolid3D</h1>
+			  </div>
+		
+		<div>
+
+		  <ul class="nav nav-tabs" role="tablist">
+			<li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Página Inicial</a></li>
+			<li role="presentation"><a href="#Produtos" aria-controls="profile" role="tab" data-toggle="tab">Produtos</a></li>
+			<li role="presentation"><a href="#Carrinho" aria-controls="messages" role="tab" data-toggle="tab">Carrinho de Compras</a></li>
+			<li role="presentation"><a href="#Conta" aria-controls="settings" role="tab" data-toggle="tab">Minha Conta</a></li>
+			<li role="presentation"><a href="#Privacidade" aria-controls="settings" role="tab" data-toggle="tab">Política de Privacidade</a></li>
+			<li role="presentation"><a href="#Contato" aria-controls="settings" role="tab" data-toggle="tab">Contate-nos</a></li>
+		  </ul>
+
+		  <div class="tab-content">
+			  
+			<div role="tabpanel" class="tab-pane active" id="home">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur metus nunc, auctor elementum tortor vel, 
+				accumsan tincidunt eros. Fusce a justo mattis ex tempor sagittis a eget tortor. Pellentesque eu placerat velit. 
+				Sed maximus maximus magna, ut rhoncus nisi bibendum et. Maecenas dignissim lorem mollis elit fermentum, sed 
+				lacinia nunc lacinia. Donec pellentesque mattis lacus, nec pulvinar lorem imperdiet at. Sed dapibus tellus sed 
+				euismod volutpat. Nulla scelerisque sapien tincidunt bibendum consequat. Pellentesque habitant morbi tristique 
+				senectus et netus et malesuada fames ac turpis egestas. Nunc luctus mollis urna sed accumsan. Donec convallis velit 
+				a metus ullamcorper maximus. Cras commodo viverra justo id tincidunt.
+			</div>
+			
+			<div role="tabpanel" class="tab-pane" id="Produtos">
+				Praesent vel ligula eu turpis dapibus accumsan auctor convallis lorem. Pellentesque dictum mattis dui, ac dapibus 
+				dolor molestie non. Nam finibus, dolor id varius ultrices, mi orci aliquam enim, ut varius libero felis eleifend dui. 
+				Aenean vitae tellus orci. Proin tempus sapien ut sem malesuada consequat sit amet et turpis. Fusce ligula nunc, rhoncus 
+				id gravida et, commodo aliquam lectus. Ut efficitur dui ut interdum malesuada. Sed convallis mattis malesuada. 
+				Vestibulum a orci placerat, ultrices magna ut, egestas enim. Nam at pellentesque diam. Duis molestie vehicula neque, ac 
+				auctor felis rutrum at. Ut consectetur eleifend leo. Aliquam erat volutpat. Nullam efficitur, magna at consectetur aliquam, 
+				arcu orci efficitur urna, sit amet interdum ex orci vel enim. Pellentesque malesuada dui urna, a rhoncus justo porta in.
+			</div>
+				
+			<div role="tabpanel" class="tab-pane" id="Carrinho">
+				Fusce velit turpis, cursus non dignissim sed, malesuada a velit. Nulla dictum vitae ante sed blandit. Fusce placerat, risus 
+				ac scelerisque rhoncus, dui nisl volutpat leo, vel accumsan mauris tellus id nisl. Cras condimentum vitae est egestas interdum. 
+				Sed nec tellus eget lacus varius accumsan vitae at purus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, 
+				per inceptos himenaeos. Integer cursus nunc feugiat pellentesque pellentesque. Mauris et pulvinar purus. Vestibulum ex erat, 
+				vehicula non pretium quis, ultrices nec velit. In hac habitasse platea dictumst. Sed iaculis justo lacus.
+			</div>
+			
+			<div role="tabpanel" class="tab-pane" id="Conta">
+				Phasellus eu mi nunc. In a ipsum a velit volutpat porttitor quis ac felis. Aenean tempor ultricies ante. Sed dignissim aliquam 
+				lorem, ac semper arcu pretium ut. Nam magna purus, facilisis at consequat sit amet, vestibulum ut orci. Nunc at varius purus. 
+				Maecenas orci arcu, convallis id lobortis a, venenatis sodales felis. Vestibulum convallis leo urna. Cras diam ex, egestas sit 
+				amet tincidunt vitae, lacinia id nunc. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer accumsan tincidunt 
+				ultrices. Aliquam consectetur, lorem ultricies gravida elementum, nibh nibh sodales turpis, in pellentesque quam metus sed 
+				purus. Integer viverra ultrices neque sed varius. Aliquam id lacinia metus, sed iaculis elit.
+			</div>
+			
+			<div role="tabpanel" class="tab-pane" id="Privacidade">
+				Cras nec arcu diam. In odio ante, efficitur sed odio vel, imperdiet ornare tellus. Proin ullamcorper velit ac est lacinia 
+				vestibulum. Duis eget interdum mauris, sodales ornare mi. Cras eleifend sem risus, egestas mattis enim porta quis. In id ante 
+				augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vulputate, ex in tincidunt tincidunt, lacus ipsum 
+				fermentum dui, sed rutrum libero nisi nec felis. Donec malesuada, nibh eu pulvinar porttitor, mauris dolor facilisis lacus, eu 
+				sagittis purus lorem id lectus. Donec rutrum non turpis ut egestas.
+			</div>
+				
+			<div role="tabpanel" class="tab-pane" id="Contato">
+				Integer mollis ipsum et lorem volutpat ullamcorper. Integer bibendum risus laoreet, eleifend massa non, aliquet velit. 
+				Maecenas laoreet, nibh eu tristique rhoncus, justo purus euismod lectus, vel imperdiet tortor est eget nisi. Proin non dapibus 
+				ipsum. Ut non ultrices lorem, quis tempus metus. Nullam pharetra nisi eu mauris consectetur, at rutrum mi sodales. Donec ac 
+				orci et nisl pulvinar aliquam. Mauris ornare quis lectus sed mollis. Duis quis sem id dolor imperdiet posuere. Aliquam pretium 
+				orci tempor justo dictum, sed sagittis purus ornare. Interdum et malesuada fames ac ante ipsum primis in faucibus. Quisque non 
+				vulputate turpis. Proin rutrum non massa vulputate mattis. Nulla facilisi. Pellentesque habitant morbi tristique senectus et 
+				netus et malesuada fames ac turpis egestas.
+			</div>
+	
+		</div>
+			
+	</div>
+	
+</div>
+	
+	
+	<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+
+	<footer>
+		<p>&copy; 2024 by a5111h software solutions - Todos os direitos reservados.</p>
+	</footer>
+</body>
+</html>
